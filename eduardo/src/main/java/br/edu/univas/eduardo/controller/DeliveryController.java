4982be31c630c0d801d61f1eb2d73dc5d42ae543
Eduardo-FCS <eduardofariacaetano100@gmail.com>
2023-04-14 21:15:42 -0300

    Adicionando create e findById

--- eduardo/src/main/java/br/edu/univas/eduardo/controller/DeliveryController.java
+++ eduardo/src/main/java/br/edu/univas/eduardo/controller/DeliveryController.java
@@ -1,5 +1,37 @@
 package br.edu.univas.eduardo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.univas.eduardo.entities.DeliveryEntity;
+import br.edu.univas.eduardo.service.DeliveryService;
+
+@RestController
+@RequestMapping("/delivery")
 public class DeliveryController {
+	
+	@Autowired
+	private DeliveryService service;
+	
+	@PostMapping("")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createProduct(@RequestBody DeliveryEntity delivery) {
+		service.createProduct(delivery);
+	}
+	
+	@GetMapping("/{code}")
+	public ResponseEntity<DeliveryEntity> getDeliveryById(@PathVariable Long code) {
+		DeliveryEntity ent = new DeliveryEntity();
+		ent = service.findById(code);
+		return ResponseEntity.ok().body(ent);
+	}
 
 }
--- eduardo/src/main/java/br/edu/univas/eduardo/service/DeliveryService.java
+++ eduardo/src/main/java/br/edu/univas/eduardo/service/DeliveryService.java
@@ -1,5 +1,30 @@
 package br.edu.univas.eduardo.service;
 
-public class DeliveryService {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.univas.eduardo.entities.DeliveryEntity;
+import br.edu.univas.eduardo.repositories.DeliveryRepository;
+
+@Service
+public class DeliveryService {
+	
+	private DeliveryRepository repo;
+	
+	@Autowired
+	public DeliveryService(DeliveryRepository del) {
+		this.repo = del;
+	}
+	
+	public void createProduct(DeliveryEntity delivery) {
+		repo.save(delivery);
+	}
+	
+	public DeliveryEntity findById(long code) {
+		Optional<DeliveryEntity> obj = repo.findById(code);
+		DeliveryEntity entity = obj.orElseThrow(() -> new RuntimeException());
+		return entity;
+	}
 }
